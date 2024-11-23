e720222eff11d4ebaa00267f2d0f71ee11e1c962
ludic <notetakingforeggs@gmail.com>
2024-11-23 09:28:03 +0000

    Feat: belated commit - mostly finished

---
+++ main.js
@@ -0,0 +1,40 @@
+const container = document.querySelector(".container");
+const backup = 16;
+let input = undefined;
+const button = document.querySelector(".btn");
+
+function createGrid(input , backup){
+
+    let gridSize = input ?? backup;
+    console.log(gridSize);
+
+    for(let i = 0; i<gridSize; i++){
+        const yDiv = document.createElement("div");
+        yDiv.classList.add("yDiv");
+        container.appendChild(yDiv)
+        for(let j = 0; j<gridSize; j++){
+            const xDiv = document.createElement("div");
+            // xDiv.textContent = "X" ;
+            xDiv.classList.add("box");
+            xDiv.addEventListener("mouseover", ()=>{xDiv.style.backgroundColor = "blue";});
+            yDiv.appendChild(xDiv)
+        }
+    } 
+}
+
+function removeChildren(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+button.addEventListener("click", ()=>{
+    input=101;
+    while(input>100){
+        input = prompt("enter desired grid size - max 100");
+    }
+    removeChildren(container); 
+    createGrid(input);
+});
+
+createGrid(16);
